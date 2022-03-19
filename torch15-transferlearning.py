7a11815907870e07f8da8942ace42ea03fd3d175
ngocdung <dungdidom@gmail.com>
2022-03-19 19:02:37 +0900

    transfer learning in Pytorch

--- torch15-transferlearning.py
+++ torch15-transferlearning.py
@@ -166,12 +166,12 @@ model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=2
 # Here, we need to freeze all the network except the final layer
 # We need to set requires_grad == False to freeze the parameters so that the gradients are not computed in backward()
 model_conv = torchvision.models.resnet18(pretrained=True)
-for param in model_conv.parameters():
+for param in model_conv.parameters(): #Freeze
     param.requires_grad = False
     
 # Parameters of newly constructed modules have requires_grad = True by default
 num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, 2)
+model_conv.fc = nn.Linear(num_ftrs, 2)  # default - requires_grad = True
 
 model_conv = model_conv.to(device)
 
@@ -181,7 +181,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
              
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size = 7, gamma = 0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size = 7, gamma = 0.1)  # change learning rate
 
 model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)   
         
