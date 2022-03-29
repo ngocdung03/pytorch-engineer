586011aa55a9f55a418e85fdd0f1cb03b05391b0
ngocdung <dungdidom@gmail.com>
2022-03-29 18:25:10 +0900

    Pytorch lightning

--- torch21-pytorchlightning.py
+++ torch21-pytorchlightning.py
@@ -41,21 +41,21 @@ class LitNeuralNet(pl.LightningModule):
         # no activation and no softmax at the end
         return out
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx):  # Added
         images, labels = batch
-        images = images.reshape(-1, 28 * 28)
+        images = images.reshape(-1, 28 * 28)   #.to(device)
 
         # Forward pass
         outputs = self(images)
         loss = F.cross_entropy(outputs, labels)
         
-        tensorboard_logs = {'train_loss': loss}
+        tensorboard_logs = {'train_loss': loss}  #'log': saving checkpoints
         # use key 'log'
-        return {"loss": loss, 'log': tensorboard_logs}
+        return {"loss": loss, 'log': tensorboard_logs}   
 
     # define what happens for testing here
 
-    def train_dataloader(self):
+    def train_dataloader(self):                 # Added
         # MNIST dataset
         train_dataset = torchvision.datasets.MNIST(
             root="./data", train=True, transform=transforms.ToTensor(), download=True
@@ -66,7 +66,7 @@ class LitNeuralNet(pl.LightningModule):
         )
         return train_loader
 
-    def val_dataloader(self):
+    def val_dataloader(self):      # Added, should be splitted from the train dataset instead
         test_dataset = torchvision.datasets.MNIST(
             root="./data", train=False, transform=transforms.ToTensor()
         )
@@ -76,7 +76,7 @@ class LitNeuralNet(pl.LightningModule):
         )
         return test_loader
     
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx):    # Added
         images, labels = batch
         images = images.reshape(-1, 28 * 28)
 
@@ -86,24 +86,24 @@ class LitNeuralNet(pl.LightningModule):
         loss = F.cross_entropy(outputs, labels)
         return {"val_loss": loss}
     
-    def validation_epoch_end(self, outputs):
+    def validation_epoch_end(self, outputs):      # Added; executed after each validation epoch
         # outputs = list of dictionaries
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         tensorboard_logs = {'avg_val_loss': avg_loss}
         # use key 'log'
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}  # calculate avg loss
     
     
-    def configure_optimizers(self):
+    def configure_optimizers(self):           # Added
         return torch.optim.Adam(self.parameters(), lr=learning_rate)
 
 if __name__ == '__main__':
     model = LitNeuralNet(input_size, hidden_size, num_classes)
     
     # gpus=8
-    # fast_dev_run=True -> runs single batch through training and validation
+    # Trainer(fast_dev_run=True) -> runs single batch through training and validation
     # train_percent_check=0.1 -> train only on 10% of data
-    trainer = Trainer(max_epochs=num_epochs)
+    trainer = Trainer(max_epochs=num_epochs)   # gpus=1, gradient_clip_val
     trainer.fit(model)
           
     # advanced features
@@ -116,3 +116,7 @@ if __name__ == '__main__':
     # auto_lr_find: automatically finds a good learning rate before training
     # deterministic: makes training reproducable
     # gradient_clip_val: 0 default
+    
+    
+    # python torch21~.py
+    # On terminal: tensorboard --logdir=lightning_logs
